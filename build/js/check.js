acc6b397e2ee321ef8b53ddd8c534f7c2e0ed0da
Luardi <poidamark@gmail.com>
2016-08-05 16:58:53 +0300

    Update check.js

--- build/js/check.js
+++ build/js/check.js
@@ -6,22 +6,22 @@ function getMessage(a,b) {
   if (typeof a === "number") {
     return "Переданное SVG-изображение содержит " + a + " объектов и " + (b * 4) + " атрибутов"
   }
-  if (typeof a === "object" & typeof b === "object") {
+  if (typeof a === "object" && typeof b === "object") {
     return "Общая площадь артефактов сжатия: " + artifactsSquare(a,b) + " пикселей"
   }
-  if (typeof a === "object" & typeof b !== "object") {
+  if (typeof a === "object" && typeof b !== "object") {
     return "Количество красных точек во всех строчках изображения: " + amountOfRedPoints(a)
   }
   function amountOfRedPoints (a) {
+    var sum = 0;
     for (i=0;i<a.length;i++) {
-      var sum = 0;
       sum += a[i];
       return sum;
     }
   }
   function artifactsSquare (a,b) {
+    var sum = 0;
     for (i=0;i<a.length;i++) {
-      var sum = 0;
       sum += a[i] * b[i];
       return sum;
     }
