bae3358375fbeeac1babdfceea3b4ccb8b66691d
Luardi <poidamark@gmail.com>
2016-08-05 15:34:57 +0300

    Update check.js

--- build/js/check.js
+++ build/js/check.js
@@ -1,16 +1,16 @@
 function getMessage(a,b) {
   if (typeof a === "boolean") {
-    if (a) {return "Переданное GIF-изображение анимировано и содержит [b] кадров"}
+    if (a) {return "Переданное GIF-изображение анимировано и содержит " + b + " кадров"}
     else return "Переданное GIF-изображение не анимировано"
   }
   if (typeof a === "number") {
-    return "Переданное SVG-изображение содержит [a] объектов и [b * 4] атрибутов"
+    return "Переданное SVG-изображение содержит " + a + " объектов и " + (b * 4) + " атрибутов"
   }
   if (typeof a === "object" & typeof b === "object") {
-    return "Общая площадь артефактов сжатия: [artifactsSquare] пикселей"
+    return "Общая площадь артефактов сжатия: " + artifactsSquare(a,b) + " пикселей"
   }
   if (typeof a === "object" & typeof b !== "object") {
-    return "Количество красных точек во всех строчках изображения: [amountOfRedPoints]"
+    return "Количество красных точек во всех строчках изображения: " + amountOfRedPoints(a)
   }
   var amountOfRedPoints = function (a) {
     for (i=0;i<a.length;i++) {
